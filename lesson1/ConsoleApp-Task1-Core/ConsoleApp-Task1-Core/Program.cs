45be44c02e3ef8f6aafbce5658a84e54c2d0015f
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2021-02-18 14:30:36 +0200

    lesson1-DZ

---
+++ lesson1/ConsoleApp-Task1-Core/ConsoleApp-Task1-Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using StandardTask2;
+using System;
+using System.Linq;
+
+namespace ConsoleApp_Task1_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = args.Any() ? args[0] : "Empty Name";
+            var greetings = Utilities.EnrichString(name);
+            Console.WriteLine(greetings);
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ lesson1/StandardTask2/StandardTask2/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StandardTask2
+{
+    public class Utilities
+    {
+        public static string EnrichString(string name)
+        {
+            var enrichString = $"{DateTime.UtcNow}: Hello {name}";
+            return enrichString;
+        }
+    }
+}
