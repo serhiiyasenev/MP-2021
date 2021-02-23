cefb8cb445cbe2bea3dbaf78ede5016a2a8b98ba
serhiiyasenev <serhii.yasenev@gmail.com>
2021-02-23 18:24:37 +0200

    lesson1-DZ (#1)
    * lesson1-DZ

---
+++ lesson1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using StandardTask2;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var name = textBox1.Text.Any() ? textBox1.Text : "Empty Name";
+            var greetings = Utilities.EnrichString(name);
+
+            MessageBox.Show(greetings);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
