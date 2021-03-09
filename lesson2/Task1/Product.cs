b05dda105152b04752e97800debe55869240f459
serhiiyasenev <serhii.yasenev@gmail.com>
2021-03-09 17:26:46 +0200

    feature/lesson 2 (#2)
    * lesson1-DZ
    
    * upd Shared
    
    * upd sln
    
    * upd
    
    * fix comment
    
    * dz 2
    
    Co-authored-by: Serhii Yasenev <serhii.yasenev@ihsmarkit.com>

---
+++ lesson2/Task1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product : IEquatable<Product>
+    {
+       private string _name;
+
+        public Product(string name, double price)
+        {
+
+            Name = name;
+            Price = price;
+        }
+
+        
+        public string Name
+        {
+            get => _name;
+            set => _name = value ?? throw new ArgumentNullException(nameof(value), "Cannot set to null");
+        }
+
+        public double Price { get; set; }
+
+
+        public bool Equals(Product other)
+        {
+            return Name.Equals(other?.Name) && Price.Equals(other?.Price);
+        }
+
+    }
+}
---
+++ lesson2/Task1/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Sorts an array in ascending order using bubble sort.
+        /// </summary>
+        /// <param name="array">Numbers to sort. Should not be null</param>
+        public static void BubbleSort(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            var length = array.Length;
+
+            for (var j = 0; j < length; j++)
+            {
+                for (var i = 0; i < length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        var x = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = x;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// based on a <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="products">Products used for searching.</param>
+        /// <param name="predicate">Product predicate.</param>
+        /// <returns>If match found then returns index of product in <paramref name="products"/>
+        /// otherwise -1.</returns>
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+
+            if (products == null)
+            {
+                throw new ArgumentNullException(nameof(products));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            for (var i = 0; i < products.Length; i++)
+            {
+                var product = products[i];
+                var isResultTrue = predicate(product);
+                if (isResultTrue)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
