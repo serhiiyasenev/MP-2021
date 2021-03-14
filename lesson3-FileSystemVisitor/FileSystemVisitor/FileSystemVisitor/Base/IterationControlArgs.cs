7a193a14dd8f2f920026cb51eb00225cd29e2363
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2021-03-14 15:13:28 +0200

    init

---
+++ lesson3-FileSystemVisitor/FileSystemVisitor/FileSystemVisitor/Base/IterationControlArgs.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace FileSystemVisitor.Base
+{
+    public class IterationControlArgs
+    {
+        public FileSystemInfo CurrentFile { get; set; }
+        public bool TerminateSearch { get; set; }
+        public bool Exclude { get; set; }
+    }
+}
