a2dd5cf1059b8609102f7c5bb74f2b181fb36ae6
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2021-04-07 20:45:57 +0300

    upd

--- lesson4-ExceptionHandling/Task1/Program.cs
+++ lesson4-ExceptionHandling/Task1/Program.cs
@@ -1,27 +1,10 @@
-﻿using System;
-
-namespace Task1
+﻿namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            string input;
-            do
-            {
-                Console.WriteLine("To finish the program, please, input STOP, to proceed - any other");
-                Console.WriteLine("Please, input your string:");
-
-                input = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(input) || string.IsNullOrEmpty(input))
-                {
-                    Console.WriteLine("String should NOT be null, empty or whitespace!");
-                }
-                else
-                {
-                    Console.WriteLine(input[0]);
-                }
-            } while (input != null && !input.Equals("STOP"));
+            
  
         }
     }
