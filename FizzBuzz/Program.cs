d90289562eef7299631af75090f7121b8da0ce2c
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2021-05-09 17:13:06 +0300

    add FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string FizzBuzz(int x)
+            {
+                return (x % 3 == 0, x % 5 == 0) switch
+                {
+                    (true, true) => "FizzBuzz",
+                    (true, _)    => "Fizz",
+                    (_, true)    => "Buzz",
+                     _           => x.ToString()
+                };
+            }
+
+            Enumerable.Range(1, 100).Select(FizzBuzz).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
