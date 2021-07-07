26dc2c33f3ed8653d20f38bb5ea06a548c74b97e
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2021-07-07 23:01:44 +0300

    init

---
+++ lesson9-Logging/Task1/BrainstormSessions/Startup.cs
@@ -0,0 +1,84 @@
+using BrainstormSessions.Core.Interfaces;
+using BrainstormSessions.Core.Model;
+using BrainstormSessions.Infrastructure;
+using log4net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainstormSessions
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(
+                optionsBuilder => optionsBuilder.UseInMemoryDatabase("InMemoryDb"));
+
+            services.AddControllersWithViews();
+
+            services.AddScoped<IBrainstormSessionRepository,
+                EFStormSessionRepository>();
+
+            services.AddScoped(factory => LogManager.GetLogger(GetType()));
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IServiceProvider serviceProvider,
+            ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                var repository = serviceProvider.GetRequiredService<IBrainstormSessionRepository>();
+
+                InitializeDatabaseAsync(repository).Wait();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            loggerFactory.CreateLogger<ILoggerFactory>();
+        }
+
+        public async Task InitializeDatabaseAsync(IBrainstormSessionRepository repo)
+        {
+            var sessionList = await repo.ListAsync();
+            if (!sessionList.Any())
+            {
+                await repo.AddAsync(GetTestSession());
+            }
+        }
+
+        public static BrainstormSession GetTestSession()
+        {
+            var session = new BrainstormSession()
+            {
+                Name = "Test Session 1",
+                DateCreated = new DateTime(2016, 8, 1)
+            };
+            var idea = new Idea()
+            {
+                DateCreated = new DateTime(2016, 8, 1),
+                Description = "Totally awesome idea",
+                Name = "Awesome idea"
+            };
+            session.AddIdea(idea);
+            return session;
+        }
+    }
+}
