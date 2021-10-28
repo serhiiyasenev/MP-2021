65a131a955127514a54b2e09aecf273578defe21
serhiiyasenev <serhii.yasenev@gmail.com>
2021-10-28 16:05:01 +0300

    Update lesson6-Reflection/Task1/Container.cs
    Co-authored-by: Yevhenii Nauholnykh <yevhenii_nauholnykh@epam.com>

--- lesson6-Reflection/Task1/Container.cs
+++ lesson6-Reflection/Task1/Container.cs
@@ -19,7 +19,14 @@ namespace Task1
                 t.GetFields().Any(f => Attribute.IsDefined(f, typeof(ImportAttribute))));
 
             foreach (var type in mappedTypes)
-                if (type.GetCustomAttributes(typeof(ExportAttribute)).FirstOrDefault() == null)
+		{
+			var contract = (type.GetCustomAttributes(typeof(ExportAttribute)).FirstOrDefault() as ExportAttribute)
+				?.Contract;
+			if (contract != null)
+				AddType(type, contract);
+			else
+				AddType(type);
+		}
                     AddType(type);
                 else if ((type.GetCustomAttributes(typeof(ExportAttribute)).FirstOrDefault() as ExportAttribute)?.Contract != null)
                     AddType(type,
