45c0bfa9922270a68e36bda6577a3bdaa955ec17
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-01-06 16:40:36 +0200

    palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------START---------");
+            Console.WriteLine();
+
+            var words = new[] {"kazak", "potop", "radar", "zakaz", "madam", "nurses run", "Palindrome", "TESTS", "false"};
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"Is Palindrome '{word}': {IsPalindrome(word)}");
+            }
+
+            Console.Read();
+        }
+
+        static bool IsPalindrome(string inputString)
+        {
+            var str = inputString.Replace(" ", string.Empty);
+
+            var firstPart = str.Substring(0, str.Length / 2);
+            char[] arrChars = str.ToCharArray();
+            
+            //Array.Reverse(arrChars);
+
+            for (int i = 0; i < arrChars.Length / 2; i++)
+            {
+                var temp = arrChars[i];
+                arrChars[i] = arrChars[arrChars.Length - i - 1];
+                arrChars[arrChars.Length - i - 1] = temp;
+            }
+
+            var secondPart = new string(arrChars);
+            secondPart = secondPart.Substring(0, secondPart.Length / 2);
+
+            return firstPart.Equals(secondPart);
+        }
+    }
+}
