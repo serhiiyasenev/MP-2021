3f726c9a9ed0be8254278d49bb2810d3cb191d88
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-01-21 16:37:43 +0200

    add projects

---
+++ WebApiAndApp/DemoWebApp3/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DemoWebApp3
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureCelsius { get; set; }
+
+        public int TemperatureFahrenheit => 32 + (int) (TemperatureCelsius / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
---
+++ fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int>{1, 1};
+
+            while (fibonacciNumbers.Count <= 10)
+            {
+                var index = ^1;
+                var current = fibonacciNumbers[index];
+                var previous = fibonacciNumbers[^2];
+
+                fibonacciNumbers.Add(current + previous);
+            }
+
+            fibonacciNumbers.ForEach(Console.WriteLine);
+            Console.Read();
+        }
+    }
+}
